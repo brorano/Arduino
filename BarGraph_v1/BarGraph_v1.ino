3ec3fd6cafd6ed479f3dd25842c5831deb661605
brorano <45371979+brorano@users.noreply.github.com>
2019-09-10 14:43:17 +0200

    Add files via upload

---
+++ BarGraph_v1/BarGraph_v1.ino
@@ -0,0 +1,36 @@
+const int ledCount = 8;
+
+int pinArray[] = {
+  1, 2, 3, 4, 5, 6, 7, 8
+  };
+
+void setup(){
+  //loop the pin array and set all to output:
+  for (int thisLed = 0; thisLed <= ledCount; thisLed++) {
+    pinMode(pinArray[thisLed], OUTPUT);
+  }
+}
+
+void loop() {
+  //leds on
+ for (int thisLed = 0; thisLed <= ledCount; thisLed++) {
+  digitalWrite(pinArray[thisLed], HIGH);
+  delay(30);
+}
+  //fuck it, leds off
+ for (int thisLed = 0; thisLed <= ledCount; thisLed++) {
+  digitalWrite(pinArray[thisLed], LOW);
+  delay(30);
+ }
+    //leds on
+ for (int thisLed = ledCount; thisLed >= 0; thisLed--) {
+  digitalWrite(pinArray[thisLed], HIGH);
+  delay(30);
+  }
+  //fuck it, leds off
+ for (int thisLed = ledCount; thisLed >= 0; thisLed--) {
+  digitalWrite(pinArray[thisLed], LOW);
+  delay(30);
+  }
+  delay(30);
+}
